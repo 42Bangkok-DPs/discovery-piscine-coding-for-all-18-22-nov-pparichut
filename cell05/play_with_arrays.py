5059a0284fa7ecd48f228aae45bbfc03c823031d
pparichut <67030141@kmitl.ac.th>
2024-11-22 12:28:53 +0700

    1

---
+++ cell05/play_with_arrays.py
@@ -0,0 +1,5 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = list({x + 2 for x in original_array if x > 5})
+
+print("Original array:", original_array)
+print("New array with 2 added to values greater than 5 (duplicates removed):", new_array)
